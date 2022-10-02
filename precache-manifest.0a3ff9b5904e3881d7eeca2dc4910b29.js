d83af7b84297885f7576970cb6b166d5eb429e7c
Quadri Alameen <quadrameen@gmail.com>
2022-10-02 12:04:58 +0100

    Updates

---
+++ precache-manifest.0a3ff9b5904e3881d7eeca2dc4910b29.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "de6171f7158d12900943",
+    "url": "/advice-generator-app/static/css/main.2c4ab02a.chunk.css"
+  },
+  {
+    "revision": "de6171f7158d12900943",
+    "url": "/advice-generator-app/static/js/main.de6171f7.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/advice-generator-app/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "8f56d7c3e3fa4c8b3d6e",
+    "url": "/advice-generator-app/static/css/2.aaaf7985.chunk.css"
+  },
+  {
+    "revision": "8f56d7c3e3fa4c8b3d6e",
+    "url": "/advice-generator-app/static/js/2.8f56d7c3.chunk.js"
+  },
+  {
+    "revision": "504a28d91370ea534000",
+    "url": "/advice-generator-app/static/js/runtime~main.504a28d9.js"
+  },
+  {
+    "revision": "3c1696ee35673310f9829b05427d8023",
+    "url": "/advice-generator-app/static/media/icon-dice.3c1696ee.svg"
+  },
+  {
+    "revision": "afbe147e3feefda674d629dd1e8b288d",
+    "url": "/advice-generator-app/static/media/pattern-divider-mobile.afbe147e.svg"
+  },
+  {
+    "revision": "477ba7b7f5debf8e71c1bd0dc138c5df",
+    "url": "/advice-generator-app/index.html"
+  }
+];
